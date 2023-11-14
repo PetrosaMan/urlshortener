134dc0eba78f06f0ce472c3493d1f964245979b8
dwt <dwt@Ubuntu.myguest.virtualbox.org>
2023-11-14 10:20:32 +0200

    First commit

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors');
-const app = express();
+const validator = require('validator');
 
-// Basic Configuration
+
+const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(cors());
-
+app.use(express.urlencoded({extended: false}));
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// URL validation
+const validateUrl = (req, res, next) => {
+  const { url } = req.body;
+  if (!validator.isURL(url)) {
+    return res.status(400).json({ error: "Invalid url"});
+  }
+  next();
+}
+
+
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl',validateUrl, function(req, res) {
+  console.log(req.body);
+  let original_url = req.body.url;
+  let short_url;
+  res.json({ "original_url": original_url, "short_url": "Todo" });  
+
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  let short_url = req.params.short_url
+  res.json({Hello: parseInt(short_url)});
 });
 
 app.listen(port, function() {
